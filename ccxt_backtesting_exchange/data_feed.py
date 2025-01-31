c6e7b443b3e384499a7ca4c069a9d2e1aaebf0b6
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-01-31 19:59:52 +0100

    Implements data feed

---
+++ ccxt_backtesting_exchange/data_feed.py
@@ -0,0 +1,43 @@
+import numpy as np
+import json
+
+
+class DataFeed:
+
+    def __init__(self, file_path: str):
+        """
+        Initialize the DataFeed by loading ohlcv data from a JSON file.
+
+        :param file_path: Path to the JSON file containing ohlcv data.
+        """
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+        self.__data = np.array(data, dtype=np.float64)
+
+    def get_data_between_timestamps(
+        self, start: int = None, end: int = None, limit: int = None
+    ):
+        """
+        Retrieve raw ohlcvs between two timestamps.
+
+        :param start: Start timestamp in milliseconds (inclusive).
+        :param end: End timestamp in milliseconds (exclusive).
+        :param limit: Maximum number of records to return. Return all records if None.
+        :return: A NumPy structured array containing the filtered ohlcvs.
+        """
+        if self.__data.size == 0:
+            return np.array([])
+
+        timestamps = self.__data[:, 0]  # Extract timestamps from first column
+        if start is None:
+            start = timestamps[0]
+        mask = timestamps >= start
+        if end is not None:
+            mask &= timestamps < end
+
+        filtered_data = self.__data[mask]
+
+        if limit is not None:
+            filtered_data = filtered_data[:limit]
+        return filtered_data
---
+++ tests/test_data_feed.py
@@ -0,0 +1,51 @@
+import pytest
+import numpy as np
+from ccxt_backtesting_exchange.data_feed import DataFeed
+
+
+@pytest.fixture
+def data_feed():
+    return DataFeed("./data/test-data.json")
+
+
+def test_get_data_between_timestamps(data_feed):
+    data = data_feed.get_data_between_timestamps()
+    assert len(data) == 60
+    assert isinstance(data, np.ndarray)
+
+
+def test_get_data_between_timestamps_with_valid_range(data_feed):
+    start = 1735686600000
+    end = 1735688400000
+    data = data_feed.get_data_between_timestamps(start=start, end=end)
+    assert len(data) == 30
+
+    # Ensure all timestamps are within the specified range
+    assert np.all(data[:, 0] >= start)
+    assert np.all(data[:, 0] < end)
+
+
+def test_get_data_between_timestamps_with_valid_range_and_limit(data_feed):
+    start = 1735686600000
+    end = 1735688400000
+    limit = 10
+    data = data_feed.get_data_between_timestamps(start=start, end=end, limit=limit)
+    assert len(data) == 10
+
+    # Ensure all timestamps are within the specified range
+    assert np.all(data[:, 0] >= start)
+    assert np.all(data[:, 0] < end)
+
+
+def test_get_data_between_timestamps_with_start(data_feed):
+    start = 1735686600000
+    data = data_feed.get_data_between_timestamps(start=start)
+    assert len(data) == 50
+    assert np.all(data[:, 0] >= start)
+
+
+def test_get_data_between_timestamps_with_end(data_feed):
+    end = 1735688400000
+    data = data_feed.get_data_between_timestamps(end=end)
+    assert len(data) == 40
+    assert np.all(data[:, 0] < end)
