1630391e3d58aeb805749386d5a88947c908e1e1
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-02-04 09:14:47 +0100

    refactor test fixtures to separate clock and backtester initialization

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from ccxt_backtesting_exchange.clock import Clock
 
 
 @pytest.fixture
-def backtester():
+def clock():
     clock = Clock(
         start_time=datetime(
             month=12, day=31, year=2024, hour=23, minute=30, tzinfo=timezone.utc
@@ -23,6 +23,11 @@ def backtester():
         ),
         interval=timedelta(minutes=1),
     )
+    return clock
+
+
+@pytest.fixture
+def backtester(clock):
     return Backtester(
         balances={"BTC": 1.0, "ETH": 5.0, "SOL": 10.0, "USDT": 10000.0},
         clock=clock,
