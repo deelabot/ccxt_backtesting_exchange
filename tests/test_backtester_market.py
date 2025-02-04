7e8e33f33ae45f6278ea5eb320971a6556277481
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-02-04 14:43:36 +0100

    test: add unit tests for fetch_ohlcv method with various timeframes

--- tests/test_backtester_market.py
+++ tests/test_backtester_market.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from ccxt.base.errors import BadSymbol
 
@@ -65,3 +66,36 @@ def test_backtester_fetch_tickers(
 def test_backtester_fetch_ticker_with_invalid_symbol(backtester_with_data_feed):
     with pytest.raises(BadSymbol):
         backtester_with_data_feed.fetch_ticker("INVALID/USDT")
+
+
+def test_get_ohlcv_on_base_timeframe(backtester_with_data_feed):
+    ohlcv = backtester_with_data_feed.fetch_ohlcv("BTC/USDT", "1m")
+    assert ohlcv.shape == (60, 6)
+
+
+def test_get_ohlcv_on_higher_timeframe_works(backtester_with_data_feed):
+    ohlcv = backtester_with_data_feed.fetch_ohlcv("BTC/USDT", "15m")
+    expected_ohlcv = [
+        [1735686000000, 93488.83, 93756.0, 93375.38, 93712.56, 117.78087],
+        [1735686900000, 93712.57, 93753.47, 93602.83, 93731.05, 58.09506],
+        [1735687800000, 93731.06, 93734.91, 93456.0, 93490.87, 99.67114],
+        [1735688700000, 93490.86, 93702.15, 93484.3, 93576.0, 61.03288],
+    ]  # 15m candle fetched from binance
+    assert np.allclose(expected_ohlcv, ohlcv, atol=1e-12)
+
+
+def test_get_ohlcv_on_non_traditional_timeframe_works(backtester_with_data_feed):
+    ohlcv = backtester_with_data_feed.fetch_ohlcv("BTC/USDT", "6m")
+    assert ohlcv.shape == (10, 6)
+
+
+def test_get_ohlcv_multiple_times_do_not_conflict(backtester_with_data_feed):
+    ohlcv1 = backtester_with_data_feed.fetch_ohlcv("BTC/USDT", "5m")
+    ohlcv2 = backtester_with_data_feed.fetch_ohlcv("BTC/USDT", "1m")
+    ohlcv3 = backtester_with_data_feed.fetch_ohlcv("BTC/USDT", "30m")
+    ohlcv4 = backtester_with_data_feed.fetch_ohlcv("SOL/USDT", "15m")
+
+    assert ohlcv1.shape == (12, 6)
+    assert ohlcv2.shape == (60, 6)
+    assert ohlcv3.shape == (2, 6)
+    assert ohlcv4.shape == (4, 6)
